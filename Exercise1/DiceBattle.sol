a7c2bd9c9c425be0a97a4f787a1e7c5fb14968f5
Kenneth-Mah <kennethmah@rocketmail.com>
2024-02-13 13:10:19 +0800

    feat: initial implementation of Exercise 1 DiceBattle

--- Exercise1/DiceBattle.sol
+++ Exercise1/DiceBattle.sol
@@ -20,17 +20,42 @@ contract DiceBattle {
     }
 
     function setBattlePair(address enemy) public {
-        // Require that only prev owner can allow an enemy
+        // (Ignore) Require that only prev owner can allow an enemy
         // Each player can only select one enemy
-        
+        battle_pair[msg.sender] = enemy;
     }
 
     function battle(uint256 myDice, uint256 enemyDice) public {
+        address myAddress = diceContract.getPrevOwner(myDice);
+        address enemyAddress = diceContract.getPrevOwner(enemyDice);
+
         // Require that battle_pairs align, ie each player has accepted a battle with the other
+        require((battle_pair[myAddress] == enemyAddress) && (battle_pair[enemyAddress] == myAddress));
 
         // Run battle
+        diceContract.roll(myDice);
+        diceContract.roll(enemyDice);
 
+        diceContract.stopRoll(myDice);
+        diceContract.stopRoll(enemyDice);
+
+        uint8 myDiceNumber = diceContract.getDiceNumber(myDice);
+        uint8 enemyDiceNumber = diceContract.getDiceNumber(enemyDice);
+
+        if (myDiceNumber > enemyDiceNumber) {
+            diceContract.transfer(myDice, myAddress);
+            diceContract.transfer(enemyDice, myAddress);
+        } else if (enemyDiceNumber > myDiceNumber) {
+            diceContract.transfer(myDice, enemyAddress);
+            diceContract.transfer(enemyDice, enemyAddress);
+        } else {
+            diceContract.transfer(myDice, myAddress);
+            diceContract.transfer(enemyDice, enemyAddress);
+        }
     }
 
     //Add relevant getters and setters
+    function getBattlePair(address myAddress) public view returns (address) {
+        return battle_pair[myAddress];
+    }
 }
