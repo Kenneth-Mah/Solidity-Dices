ec9e6e61e9a805d07a76dabe4f867ced649582bb
Kenneth-Mah <kennethmah@rocketmail.com>
2024-02-14 18:13:48 +0800

    feat: initial implementation of Exercise 2

---
+++ Exercise2/DiceMarket.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+import "./Dice.sol";
+import "./DiceToken.sol";
+
+contract DiceMarket {
+    
+}
--- Exercise2/DiceToken.sol
+++ Exercise2/DiceToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 import "./ERC20.sol";
 
 contract DiceToken {
+
     ERC20 erc20Contract;
     uint256 supplyLimit;
     uint256 currentSupply;
@@ -13,20 +14,15 @@ contract DiceToken {
         erc20Contract = e;
         owner = msg.sender;
         supplyLimit = 10000;
-
     }
-
 
     function getCredit() public payable {
         uint256 amt = msg.value / 10000000000000000; // Get DTs eligible
         require(erc20Contract.totalSupply() + amt < supplyLimit, "DT supply is not enough");
-        // erc20Contract.transferFrom(owner, msg.sender, amt);
         erc20Contract.mint(msg.sender, amt);
-        
     }
 
     function checkCredit() public view returns(uint256) {
         return erc20Contract.balanceOf(msg.sender);
     }
-
 }
